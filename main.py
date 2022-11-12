16080baf6fb67249637ac1cecb5aaa63a085ed9a
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-12 22:16:24 +0530

    minor edits

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ while True:
 
                         data = input("Type in the message you want to encrypt : ")
                         data = bytes(data,'utf-8')    
-                        print(data)
+                    
                         key = Fernet.generate_key()
                         f = Fernet(key)
-                        estr=f.encrypt(bytes(data))
+                        estr=f.encrypt(data)
                         print("Your Encrypted message is : " +str(estr))
                         print("Key to decode the message :" + str(key))
-                        print(data)
+
         elif "decode" in x:
-                        dstr=input("Enter the message you want to decode:")
-                        dk=input("Enter Key:")
+                        dstr=input("Enter the message you want to decode (make sure that you exclude b) :")
+                        dk=input("Enter Key (make sure that you exclude b) :")
                         y=Fernet.generate_key()
                         a=str(dstr)
                         y=(dk)
