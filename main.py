0dfbdf895de8100cf1cbdf725b4017f2d843538e
Mir Habeel Ahmad <102403483+Habeel06@users.noreply.github.com>
2022-11-12 22:01:14 +0530

    new

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 from cryptography.fernet import Fernet
+
 while True:
-    x=input("Enter 1 to encode and 2 to decode :")
-    if x==1:
 
-        ystr=input("Enter the message you want to encrypt:")
-        key = Fernet.generate_key()
-        f = Fernet(key)
-        estr=f.encrypt(b"ystr")
-        print("Your Encrypted message is : " + str(estr))
-        print("Key to decode the message :" + str(key))
-    if x==2:
-        dstr=input("Enter the message you want to decode:")
-        dk=input("Enter Key:")
 
-        a=str(dstr)
-        y=(dk)
-        x=Fernet(y)
-        decodedmsg = x.decrypt(a).decode()
-        print("The decoded message is : " + str(decodedmsg))
+        x=input("Type encode to encode and decode to decode :")
 
-    else:
-        print("Try again")
+        if "encode" in x:
 
+                        data = input("Type in the message you want to encrypt : ")
+                        data = bytes(data,'utf-8')    
+                        print(data)
+                        key = Fernet.generate_key()
+                        f = Fernet(key)
+                        estr=f.encrypt(bytes(data))
+                        print("Your Encrypted message is : " +str(estr))
+                        print("Key to decode the message :" + str(key))
+                        print(data)
+        elif "decode" in x:
+                        dstr=input("Enter the message you want to decode:")
+                        dk=input("Enter Key:")
+                        y=Fernet.generate_key()
+                        a=str(dstr)
+                        y=(dk)
+                        x=Fernet(y)
+                        decodedmsg = x.decrypt(a).decode()
+                        print("The decoded message is : " + str(decodedmsg))
+
+
+        
