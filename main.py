7267445f9a873307683b769d9e6252e9f34a22d0
Raqeem Ahmad Tak <105811418+RaxTak@users.noreply.github.com>
2022-11-14 08:02:45 +0530

    Fixed All bugs

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ while True:
         x=input("Type encode to encode and decode to decode :")
 
         if "encode" in x:
-
+                try:
                         data = input("Type in the message you want to encrypt : ")
                         data = bytes(data,'utf-8')    
                     
@@ -34,20 +34,27 @@ while True:
                         estr=f.encrypt(data)
                         print("Your Encrypted message is : " +str(estr))
                         print("Key to decode the message :" + str(key))
+                except:
+                         print("Oops! Wrong encrypted message!")
+                         print("Try Again!") 
+
+
+
+                
 
         elif "decode" in x:
-                        try:
-                                dstr=input("Enter the message you want to decode (make sure that you exclude b) :")
-                                dk=input("Enter Key (make sure that you exclude b) :")
-                                y=Fernet.generate_key()
-                                a=str(dstr)
-                                y=(dk)
-                                x=Fernet(y)
-                                decodedmsg = x.decrypt(a).decode()
-                                print("The decoded message is : " + str(decodedmsg))
-                        except ValueError:
-                                print('''Oops! Wrong encrypted message or wrong key given.
-TRY AGAIN!''')
-
-
-        
+                try:
+                        dstr=input("Enter the message you want to decode (make sure that you exclude b) :")
+                        dk=input("Enter Key (make sure that you exclude b) :")
+                        y=Fernet.generate_key()
+                        a=str(dstr)
+                        y=(dk)
+                        x=Fernet(y)
+                        decodedmsg = x.decrypt(a).decode()
+                        print("The decoded message is : " + str(decodedmsg))
+                except:
+                        print("Oops! Wrong encrypted message!")
+                        print("Try Again!")
+        else:
+                print("Wrong Key Given")
+                print("Try Again!")
